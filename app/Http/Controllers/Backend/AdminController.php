fa433ae1be571210368630f6311d4a29ff817a2a
Your Name <you@example.com>
2019-07-27 21:54:30 +0600

    Joyanta_E Comerce

---
+++ app/Http/Controllers/Backend/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+
+class AdminController extends Controller
+{
+  public function __construct()
+  {
+    $this->middleware('auth:admin');
+  }
+
+}
---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer-bottom">
+
+  <p class="text-center">&copy; 2019 All rights reserved | Joyanta Kumar Sarker</p>
+
+</footer>
